8a407915b7e1db1b91b2b383a1cdaf00e68a22aa
Eber Alejo <eber4202@hotmail.com>
2023-01-13 19:50:55 -0600

    DELTE: babel.config.cjs

--- babel.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
-  plugins: [
-    'add-module-exports',
-    '@babel/plugin-transform-async-to-generator'
-  ]
-}
