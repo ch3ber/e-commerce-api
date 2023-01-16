13f0f8c06e754086bb0e8f7c2b08ebd64f812076
Eber Alejo <eber4202@hotmail.com>
2023-01-16 14:59:35 -0600

    ADD: types

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,25 @@
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  createdAt: string;
+}
+
+
+export interface Product {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  createdAt: string;
+  categoryId: number;
+  category: Category;
+}
+
+export interface ProductDTO extends Omit< Product, 'category'> {
+}
+
 export type EmailBody = {
   from: string | undefined,
   to: string | string[], // list of receivers
